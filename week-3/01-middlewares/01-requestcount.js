a62254c6b2f45c644c74a29a653faf909a7f6a19
maheshsp1809 <maheshsp1809@gmail.com>
2024-01-28 22:14:47 +0530

    assignment 3 01 solved

--- week-3/01-middlewares/01-requestcount.js
+++ week-3/01-middlewares/01-requestcount.js
@@ -1,25 +1,39 @@
-const request = require('supertest');
-const assert = require('assert');
-const express = require('express');
+const express = require("express");
 
 const app = express();
 let requestCount = 0;
-
+export function globalMiddleware(req, res, next) {
+  requestCount = requestCount + 1;
+  next();
+}
 // You have been given an express server which has a few endpoints.
 // Your task is to create a global middleware (app.use) which will
 // maintain a count of the number of requests made to the server in the global
 // requestCount variable
 
-app.get('/user', function(req, res) {
-  res.status(200).json({ name: 'john' });
+app.use(globalMiddleware);
+
+// Routes
+app.get("/user", function (req, res) {
+  res.status(200).json({ name: "john" });
 });
 
-app.post('/user', function(req, res) {
-  res.status(200).json({ msg: 'created dummy user' });
+app.post("/user", function (req, res) {
+  res.status(200).json({ msg: "created dummy user" });
 });
 
-app.get('/requestCount', function(req, res) {
+app.get("/requestCount", function (req, res) {
   res.status(200).json({ requestCount });
+});
+
+// Global Error Handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: "Something went wrong!" });
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
 
 module.exports = app;
